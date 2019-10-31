bb7bf10b5636d1761a952b85699cfcf49c57b977
Paul Sammut <paul@pop-os.localdomain>
2019-10-31 17:26:46 +1100

    Maintain windows in page

--- fluidShell.js
+++ fluidShell.js
@@ -3,7 +3,6 @@ const Main = imports.ui.main;
 
 const Extension = imports.misc.extensionUtils.getCurrentExtension();
 const { Page } = Extension.imports.page;
-const { Clone } = Extension.imports.clone;
 const { Log } = Extension.imports.utils.logger;
 
 const WorkspaceManager = global.workspace_manager;
@@ -13,32 +12,23 @@ var FluidShell = class FuildShell {
 
     constructor() {
 
-        this.page = new Page();
-        const windows  = new Map();
         const activeWorkspace = WorkspaceManager.get_active_workspace();
-        // activeWorkspace.connect('window-added', (workspace, metaWindow) => {
-        Display.connect('window-created', (display, metaWindow) => {
-            if (metaWindow.window_type !== Meta.WindowType.NORMAL)
-                return;
-            // const windows = activeWorkspace.list_windows();
-            // const windows = global.get_window_actors();
-            const clone = new Clone(metaWindow);
-            this.page.add_child(clone);
-            windows.set(metaWindow, {page: this.page, clone})
-        });
 
+        this.page = new Page();
         global.stage.add_child(this.page);
 
+        // activeWorkspace.connect('window-added', (workspace, metaWindow) => {
+        Display.connect('window-created', (display, metaWindow) => {
+            this.page.addWindow(metaWindow);
+        });
+    
         activeWorkspace.connect('window-removed', (workspace, metaWindow) => {
-            const {page, clone} = windows.get(metaWindow);
-            page.remove_child(clone);
-            windows.delete(metaWindow);
+            this.page.removeWindow(metaWindow);
         });
 
     }
 
     destroy() {
         global.stage.remove_child(this.page);
-        // Main.layoutManager.removeChrome(this.rightPanel);
     }
 }
--- page.js
+++ page.js
@@ -1,14 +1,31 @@
-const { GObject, Clutter } = imports.gi;
+const { GObject, Clutter, Meta } = imports.gi;
 
 const Extension = imports.misc.extensionUtils.getCurrentExtension();
 const { Grid } = Extension.imports.grid;
+const { Clone } = Extension.imports.clone;
 
 var Page = GObject.registerClass({},
     class Page extends Clutter.Actor {
         _init() {
             super._init();
             this.set_layout_manager(new Grid());
+            this.windows = new Map();
         }
+
+        addWindow(metaWindow) {
+            if (metaWindow.window_type !== Meta.WindowType.NORMAL)
+                return;
+            const clone = new Clone(metaWindow);    
+            this.add_child(clone);
+            this.windows.set(metaWindow, clone)
+
+        }
+
+        removeWindow(metaWindow) {
+            this.remove_child(this.windows.get(metaWindow));
+            this.windows.delete(metaWindow);
+        }
+
         destroy() {
             this.remove_all_children();
         }
