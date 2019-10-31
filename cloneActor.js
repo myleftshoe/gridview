8f2255a570d8bbe962a75125cef596aea61a45e2
Paul Sammut <paul@pop-os.localdomain>
2019-10-31 18:43:39 +1100

    Scaled clones

--- cloneActor.js
+++ cloneActor.js
@@ -1,6 +1,9 @@
 const { Clutter } = imports.gi;
 
-const size = [200, 100];
+const Extension = imports.misc.extensionUtils.getCurrentExtension();
+const { Log } = Extension.imports.utils.logger;
+
+const defaultHeight = 200;
 
 var CloneActor = class CloneActor extends Clutter.Actor {
 
@@ -8,7 +11,11 @@ var CloneActor = class CloneActor extends Clutter.Actor {
         super();
         const source = metaWindow.get_compositor_private();
         const clone = new Clutter.Clone({source});
-        clone.set_size(...size);
+        const height = defaultHeight;
+        // Log.properties(source);
+        const scaleFactor = clone.get_height() / height;
+        const width = clone.get_width() / scaleFactor;
+        clone.set_size(width, height);
         this.add_actor(clone);
     }
 }
