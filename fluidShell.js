2da278ec6570c9d0d12e6b43bed219f27f4c744e
Paul Sammut <paul@pop-os.localdomain>
2019-11-14 13:05:37 +1100

    .

--- fluidShell.js
+++ fluidShell.js
@@ -8,6 +8,7 @@ const { Row } = Extension.imports.row;
 const { Cell } = Extension.imports.cell;
 const { Log } = Extension.imports.utils.logger;
 const { makeSortable, unmakeSortable } = Extension.imports.sortable;
+const { makeZoomable } = Extension.imports.zoomable;
 
 const Display = global.display;
 const Stage = global.stage;
@@ -37,25 +38,7 @@ var FluidShell = GObject.registerClass({},
                 }
             });
             makeSortable();
-            // Log.properties(this.draggable);
-            this.connect('scroll-event', (source, event) => {
-                const direction = event.get_scroll_direction();
-                if (direction > 1) return;
-                let amount = 0.5;
-                const [scaleX, scaleY] = source.get_scale();
-                if (direction === Clutter.ScrollDirection.DOWN) {
-                    if (scaleX < 1) return;
-                    amount = -amount;
-                }
-                const [sx, sy] = source.get_transformed_position();
-                const [sw, sh] = source.get_transformed_size();
-                const [x, y] = event.get_coords();
-                log([x, y, sx, sy, sw, sh]);
-                // source.set_pivot_point((x - sx) / sw, (y - sy) / sh);
-                // source.set_scale(scaleX + amount, scaleY + amount);
-                source.set_scale_with_gravity(scaleX + amount, scaleY + amount, Clutter.Gravity.CENTER);
-                // this.set_size(...this.get_size());
-            });
+            makeZoomable(this);
             this.cellSignals = [];
 
             this.refresh();
---
+++ zoomable.js
@@ -0,0 +1,22 @@
+const { Clutter } = imports.gi;
+
+function makeZoomable(actor) {
+    actor.connect('scroll-event', (source, event) => {
+        const direction = event.get_scroll_direction();
+        if (direction > 1) return;
+        let amount = 0.5;
+        const [scaleX, scaleY] = source.get_scale();
+        if (direction === Clutter.ScrollDirection.DOWN) {
+            if (scaleX < 1) return;
+            amount = -amount;
+        }
+        const [sx, sy] = source.get_transformed_position();
+        const [sw, sh] = source.get_transformed_size();
+        const [x, y] = event.get_coords();
+        log([x, y, sx, sy, sw, sh]);
+        // source.set_pivot_point((x - sx) / sw, (y - sy) / sh);
+        // source.set_scale(scaleX + amount, scaleY + amount);
+        source.set_scale_with_gravity(scaleX + amount, scaleY + amount, Clutter.Gravity.CENTER);
+        // this.set_size(...this.get_size());
+    });
+}
