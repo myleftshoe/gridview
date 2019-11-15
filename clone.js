06b26c143e6e62cad7b899f856b97a3e9fb14756
Paul Sammut <paul@pop-os.localdomain>
2019-11-15 14:38:30 +1100

    zoom out animation of start

--- clone.js
+++ clone.js
@@ -1,5 +1,6 @@
-const Main = imports.ui.main;
 const { Clutter } = imports.gi;
+const Tweener = imports.ui.tweener;
+const Main = imports.ui.main;
 
 const Extension = imports.misc.extensionUtils.getCurrentExtension();
 const { Log } = Extension.imports.utils.logger;
@@ -18,9 +19,21 @@ var Clone = class Clone extends Clutter.Actor {
         // Show entire window even if part of it is offscreen.
         clone.remove_clip();
         this.add_actor(clone);
-        clone.set_scale(...initialScale)
-        const [width, height] = clone.get_size();
-        this.set_size(width * initialScale[0], height * initialScale[1]);
+        // clone.set_scale(...initialScale)
+        // const [width, height] = clone.get_size();
+        // this.set_size(width * initialScale[0], height * initialScale[1]);
+        Tweener.addTween(clone, {
+            scale_x: initialScale[0],
+            scale_y: initialScale[1],
+            delay:0,
+            time:.3,
+            transition: 'easeOutQuad',
+            onUpdate: () => {
+                const [width, height] = clone.get_size();
+                this.set_size(width * scale_x, height * clone.scale_y);
+            },
+            // onUpdateScope: this,
+        });
 
     }
     destroy() {
