a2836dfe99cb8d0b1fe5381bf2d7abaec2825acd
Paul Sammut <paul@pop-os.localdomain>
2019-11-20 09:08:25 +1100

    clone minor refactor

--- clone.js
+++ clone.js
@@ -11,26 +11,23 @@ var Clone = class Clone extends Clutter.Actor {
 
     constructor(metaWindow) {
 
-        super({reactive:false});
+        super({ reactive: false });
 
         const source = metaWindow.get_compositor_private();
-        const clone = new Clutter.Clone({source});
+        const clone = new Clutter.Clone({ source });
 
         // Show entire window even if part of it is offscreen.
         clone.remove_clip();
         this.add_actor(clone);
-        // clone.set_scale(...initialScale)
-        // const [width, height] = clone.get_size();
-        // this.set_size(width * initialScale[0], height * initialScale[1]);
         Tweener.addTween(clone, {
             scale_x: initialScale[0],
             scale_y: initialScale[1],
-            delay:0,
-            time:.3,
+            delay: 0,
+            time: .3,
             transition: 'easeOutQuad',
             onUpdate: () => {
-                const [width, height] = clone.get_size();
-                this.set_size(width * clone.scale_x, height * clone.scale_y);
+                const { width, height, scale_x, scale_y } = clone;
+                this.set_size(width * scale_x, height * scale_y);
             },
             // onUpdateScope: this,
         });
