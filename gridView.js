20a4a4f173baffd65da06f9ec2011336ef3ddd78
Paul Sammut <paul@pop-os.localdomain>
2019-11-21 10:32:34 +1100

    .

--- gridView.js
+++ gridView.js
@@ -1,4 +1,4 @@
-const { GObject, St, Shell } = imports.gi;
+const { GObject, Meta, St, Shell } = imports.gi;
 const Tweener = imports.ui.tweener;
 const Main = imports.ui.main;
 
@@ -28,10 +28,10 @@ var GridView = GObject.registerClass(
     class GridView extends St.BoxLayout {
         _init() {
             const {width, height} = Display.get_monitor_geometry(Display.get_primary_monitor());
-            super._init({ 
-                style_class, 
+            super._init({
+                style_class,
                 opacity:0,
-                vertical: true, 
+                vertical: true,
                 reactive: true,
                 width,
                 height,
@@ -44,10 +44,11 @@ var GridView = GObject.registerClass(
         }
         populate() {
             UI.workspaces.forEach((workspace) => {
-                const windows = workspace.list_windows();
+                // const windows = workspace.list_windows();
                 // const windows = UI.getWorkspaceWindows(workspace);
+                const windows = global.display.get_tab_list(Meta.TabList.NORMAL, workspace);
                 if (!windows.length) return;
-                const row = new Row();
+                const row = new Row(workspace);
                 windows.forEach(metaWindow => {
                     const cell = new Cell(metaWindow);
                     row.add_child(cell);
@@ -61,13 +62,21 @@ var GridView = GObject.registerClass(
             // windows stealing pointer events.
             Main.layoutManager.trackChrome(this);
             Main.pushModal(this, { actionMode: Shell.ActionMode.OVERVIEW })
-            Tweener.addTween(this, { 
+            Tweener.addTween(this, {
                 opacity: 255,
                 time: ANIMATION_TIME,
-                transition: 'easeOutQuad' 
-            });            
+                transition: 'easeOutQuad'
+            });
         }
         hide() {
+            this.get_children().forEach((row, rowIndex) => {
+                // log(`--- row: ${rowIndex+ 1}`);
+                [...row.get_children()].reverse().forEach(cell => {
+                    // log(cell.id);
+                    cell.metaWindow.change_workspace(row.workspace);
+                    cell.metaWindow.focus(global.get_current_time());
+                });
+            });
             Main.layoutManager.untrackChrome(this);
             Main.popModal(this);
         }
