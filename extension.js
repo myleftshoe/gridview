3da56712f5dbc555aa979893617d080afa1a1997
Paul Sammut <paul@pop-os.localdomain>
2019-11-29 09:25:51 +1100

    refactor

--- extension.js
+++ extension.js
@@ -37,49 +37,33 @@ function enable() {
     Main.layoutManager.connect('startup-complete', prepare);
 }
 
-let hotTop;
 let container;
-let scrollable;
+let gridView;
 
 function prepare() {
-    hotTop = new HotTop({width: 36});
+    const hotTop = new HotTop({width: 36});
     hotTop.connect('enter-event', (actor, event) => {
         log('enter-event')
-        if (!global.stage.contains(global.gridView)) {
-            show();
-            return;
-        }
+        show();
     });
-    const hotLeft = new HotLeft({width:13}) 
-    container = new St.Widget();
-    container.connect('key-press-event', (actor, event) => {
-        if (event.get_key_symbol() === Clutter.Escape) {
-            if (global.gridView) {
-                hide();
-            }
-        }
-    });
-    const backgroundManager = new Background.BackgroundManager({
-        monitorIndex: Main.layoutManager.primaryIndex,
-        container: container,
-        vignette: false
-    });
-    global.gridView = new GridView();
-    scrollable = new Scrollable(global.gridView,{height:10, width:Main.uiGroup.get_width()});
+    const hotLeft = new HotLeft({width:13});
+    container = createContainer();
+    gridView = new GridView();
+    const scrollable = new Scrollable(gridView,{height:10, width:Main.uiGroup.get_width()});
     container.add_child(scrollable);
     container.add_child(scrollable.scrollbar);
     show();
 }
 
 function show() {
-    if (global.stage.contains(global.gridView)) return;
-    global.gridView.populate();
+    if (global.stage.contains(container)) return;
+    gridView.populate();
     global.stage.add_child(container);
     Main.pushModal(container, { actionMode: Shell.ActionMode.OVERVIEW })
 }
 
 function hide() {
-    if (!global.stage.contains(global.gridView)) return;
+    if (!global.stage.contains(container)) return;
     Tweener.addTween(container, {
         scale_x: 1,
         scale_y: 1,
@@ -95,7 +79,20 @@ function hide() {
 function disable() {
     log(`${Extension.metadata.uuid} disable()`);
     global.display.disconnect(acceleratorSignal);
-    if (global.gridView) {
-        hide();
-    }
+    hide();
+}
+
+function createContainer() {
+    const container = new St.Widget();
+    container.connect('key-press-event', (actor, event) => {
+        if (event.get_key_symbol() === Clutter.Escape) {
+            hide();
+        }
+    });
+    const backgroundManager = new Background.BackgroundManager({
+        monitorIndex: Main.layoutManager.primaryIndex,
+        container: container,
+        vignette: false
+    });
+    return container;
 }
