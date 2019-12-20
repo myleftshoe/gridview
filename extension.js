f99bcd3409ba3f892f9c9dd5e8a62ae18fefa970
Paul Sammut <paul@pop-os.localdomain>
2019-12-20 19:02:02 +1100

    .

--- extension.js
+++ extension.js
@@ -2,7 +2,7 @@ const Main = imports.ui.main;
 const Signals = imports.signals;
 
 const Extension = imports.misc.extensionUtils.getCurrentExtension();
-const fluidWM = Extension.imports.fluidWM;
+const FluidWM = Extension.imports.fluidWM;
 
 function init() {
     log(`***************************************************************`);
@@ -18,13 +18,13 @@ function enable() {
 
 function disable() {
     log(`${Extension.metadata.uuid} disable()`);
-    fluidWM.stop();
+    FluidWM.stop();
 }
 
 function wait() {
     if (Main.layoutManager._startingUp)
-        Main.layoutManager.connect('startup-complete', fluidWM.start);
+        Main.layoutManager.connect('startup-complete', FluidWM.start);
     else
-        fluidWM.start();
+        FluidWM.start();
 }
 
